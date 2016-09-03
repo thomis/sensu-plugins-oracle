c9a2330158ca2fc52a358a798445ae986880d3c9
Thomas Steiner <thomas.steiner@ikey.ch>
2016-09-03 17:30:56 +0200

    add connect timeout option

--- bin/check-oracle-alive.rb
+++ bin/check-oracle-alive.rb
@@ -51,8 +51,17 @@ class CheckOracle < Sensu::Plugin::Check::CLI
          short: '-P PRIVILEGE',
          long: '--privilege PRIVILEGE'
 
+  option :timeout,
+         description: 'Connection timeout (seconds)',
+         short: '-T TIMEOUT',
+         long: '--timeout TIMEOUT',
+         default: nil
+
   def run
+    OCI8.properties[:connect_timeout] = config[:timeout].to_i if config[:timeout]
+
     connection = OCI8.new(config[:username], config[:password], config[:database], config[:privilege])
+
     ok "Server version: #{connection.oracle_server_version}"
   rescue OCIError => e
     critical "#{e.message.split("\n").first}"

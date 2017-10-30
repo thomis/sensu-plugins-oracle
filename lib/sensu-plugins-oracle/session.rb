71bb4d51501670cc904195f65b16d467df04fbf1
Thomas Steiner <thomas.steiner@ikey.ch>
2017-10-30 13:38:46 +0100

    ! style

--- lib/sensu-plugins-oracle/session.rb
+++ lib/sensu-plugins-oracle/session.rb
@@ -126,7 +126,10 @@ module SensuPluginsOracle
             else
               session.send(args[:method])
             end
-            puts format('Done       %s, took %0.1f ms', session.name, (Time.now - start) * 1000) if args[:config][:verbose]
+            message = format('Done       %s, took %0.1f ms',
+                             session.name,
+                             (Time.now - start) * 1000)
+            puts message if args[:config][:verbose]
           end
         end
       end

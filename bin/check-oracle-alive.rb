ab3988a7031ad6dd5d1b674391aade6eb8f52bac
Thomas Steiner <thomas.steiner@ikey.ch>
2017-10-30 13:13:09 +0100

    ! style

--- bin/check-oracle-alive.rb
+++ bin/check-oracle-alive.rb
@@ -120,9 +120,9 @@ class CheckOracleAlive < Sensu::Plugin::Check::CLI
 
   def handle_connections_from_file
     sessions = ::SensuPluginsOracle::Session.parse_from_file(config[:file])
-    ::SensuPluginsOracle::Session.handle_multiple( sessions: sessions,
+    ::SensuPluginsOracle::Session.handle_multiple(sessions: sessions,
                                                    method: :alive?,
-                                                   config: config )
+                                                   config: config)
 
     errors = []
     sessions.each do |session|

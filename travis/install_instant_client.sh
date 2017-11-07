5ef539d24bd5ed2cf3d858ca42b7436f99dbd32e
thomis <thomas.steiner@novartis.com>
2017-11-07 22:02:45 +0100

    ! if then else in script

--- travis/install_instant_client.sh
+++ travis/install_instant_client.sh
@@ -1,3 +1,12 @@
-~!/bin/sh
+#!/bin/sh
 
-echo $TRAVIS_OS_NAME
+if [ $TRAVIS_OS_NAME = "linux" ]
+then
+  echo "Installing oracle instant client for linux..."
+
+elif [ $TRAVIS_OS_NAME = "osx" ] then
+  echo "Installing oracle instant client for linux..."
+
+else
+  echo "Don't know how to install oracle instant client for '$TRAVIS_OS_NAME'"
+fi

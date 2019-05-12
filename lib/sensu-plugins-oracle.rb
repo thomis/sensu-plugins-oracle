9a730c92c0eb08048872fd2b4c9ff65c77cb1e07
Steiner, Thomas <thomas.steiner@ikey.ch>
2019-05-12 18:32:32 +0200

    ! ruby version & syntax

--- lib/sensu-plugins-oracle.rb
+++ lib/sensu-plugins-oracle.rb
@@ -1,2 +1,5 @@
+require 'oci8'
+require 'dentaku'
+
 require 'sensu-plugins-oracle/version'
 require 'sensu-plugins-oracle/session'
--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -14,12 +14,12 @@ describe SensuPluginsOracle::Session do
       @session = SensuPluginsOracle::Session.new(connect_string: "a/b@c", name: 'a_name')
     end
 
-    it "creates a session" do
+    it 'creates a session' do
       expect(@session.name).to eq('a_name')
       expect(@session.connect_string).to eq('a/b@c')
     end
 
-    it "handles invalid aliveness" do
+    it 'handles invalid aliveness' do
       expect(@session.alive?).to eq(false)
       expect(@session.error_message).to eq('a_name: ORA-12154: TNS:could not resolve the connect identifier specified')
     end
@@ -89,7 +89,7 @@ describe SensuPluginsOracle::Session do
       @session = SensuPluginsOracle::Session.new(username: 'a', password: 'b', database: 'c', priviledge: 'SYSDBA', name: 'a_name')
     end
 
-    it "creates a session" do
+    it 'creates a session' do
       expect(@session.name).to eq('a_name')
       expect(@session.username).to eq('a')
       expect(@session.password).to eq('b')
@@ -97,7 +97,7 @@ describe SensuPluginsOracle::Session do
       expect(@session.priviledge).to eq(:SYSDBA)
     end
 
-    it "handles invalid aliveness" do
+    it 'handles invalid aliveness' do
       expect(@session.alive?).to eq(false)
       expect(@session.error_message).to eq('a_name: ORA-12154: TNS:could not resolve the connect identifier specified')
     end

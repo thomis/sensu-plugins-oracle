1e83703b2ee3d6f0b5da2a4cebb398ad2d7283b5
Steiner, Thomas <thomas.steiner@ikey.ch>
2019-05-12 19:59:56 +0200

    Update install_instant_client.sh

--- .travis/install_instant_client.sh
+++ .travis/install_instant_client.sh
@@ -1,12 +1,15 @@
--#!/bin/sh
+#!/usr/bin/env bash
 
 if [ $TRAVIS_OS_NAME = "linux" ]
 then
   echo "Installing oracle instant client for linux..."
 
-  cd .travis/oracle
-  apt-get install -y oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm
-  apt-get install -y oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm
+  cd "$(dirname "$(readlink -f "$0")")"
+
+  echo $(pwd)
+
+  sudo apt-get install -y oracle/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm
+  sudo apt-get install -y oracle/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm
 
 else
   echo "Don't know how to install oracle instant client for '$TRAVIS_OS_NAME'"

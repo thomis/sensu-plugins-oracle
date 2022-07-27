eda79f56c1e96791aa2d066a994579ef4d45d679
thomis <thomas.steiner@ikey.ch>
2022-07-27 21:11:02 +0200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require "simplecov"
+SimpleCov.start
+
 require_relative "../lib/sensu-plugins-oracle"
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -8,6 +12,16 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  config.profile_examples = 10
   config.order = :random
-  Kernel.srand config.see
+  Kernel.srand config.seed
 end

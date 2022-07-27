8b68fda3e32cde460d753428381aba69060f51ef
thomis <thomas.steiner@ikey.ch>
2022-07-27 21:39:14 +0200

    ! change rspecs and add more tests

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -9,65 +9,71 @@ RSpec.describe SensuPluginsOracle::Session do
   end
 
   context "connect string" do
-    before(:each) do
-      @session = SensuPluginsOracle::Session.new(connect_string: "a/b@c", name: "a_name", module: "m")
-    end
+    let!(:session) {
+      SensuPluginsOracle::Session.new(connect_string: "a/b@c", name: "a_name", module: "m")
+    }
 
     it "creates a session" do
-      expect(@session.name).to eq("a_name")
-      expect(@session.connect_string).to eq("a/b@c")
-      expect(@session.module).to eq("m")
+      expect(session.name).to eq("a_name")
+      expect(session.connect_string).to eq("a/b@c")
+      expect(session.module).to eq("m")
+    end
+
+    it "allows to set timeout" do
+      expect{
+        SensuPluginsOracle::Session.timeout_properties(30)
+      }.not_to raise_error
     end
 
     it "handles invalid aliveness" do
-      expect(@session.alive?).to eq(false)
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      expect(session.alive?).to eq(false)
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
 
     it "return error message" do
-      @session.alive?
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      session.alive?
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
 
     it "handles invalid query" do
-      expect(@session.query("select 1 from dual")).to eq(false)
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      expect(session.query("select 1 from dual")).to eq(false)
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
 
     it "handles empty query results" do
-      method, message = @session.handle_query_result
+      method, message = session.handle_query_result
       expect(method).to eq(:ok)
       expect(message).to eq(nil)
     end
 
     it "handles simple query results" do
-      @session.rows = [[1]]
+      session.rows = [[1]]
 
-      method, message = @session.handle_query_result(show: true)
+      method, message = session.handle_query_result(show: true)
       expect(method).to eq(:ok)
       expect(message).to eq("- 1")
     end
 
     it "handles simple query results with tuples" do
-      @session.rows = [[1]]
+      session.rows = [[1]]
 
-      method, message = @session.handle_query_result(show: true, tuples: true)
+      method, message = session.handle_query_result(show: true, tuples: true)
       expect(method).to eq(:ok)
       expect(message).to eq("- 1")
     end
 
     it "handles simple query results with critical formula" do
-      @session.rows = [[1]]
+      session.rows = [[1]]
 
-      method, message = @session.handle_query_result(show: true, critical: "value > 0")
+      method, message = session.handle_query_result(show: true, critical: "value > 0")
       expect(method).to eq(:critical)
       expect(message).to eq("- 1")
     end
 
     it "handles simple query results with tuples and warning formula" do
-      @session.rows = [[1]]
+      session.rows = [[1]]
 
-      method, message = @session.handle_query_result(show: true, tuples: true, warning: "value > 0")
+      method, message = session.handle_query_result(show: true, tuples: true, warning: "value > 0")
       expect(method).to eq(:warning)
       expect(message).to eq("- 1")
     end
@@ -83,32 +89,32 @@ RSpec.describe SensuPluginsOracle::Session do
   end
 
   context "connection with username, password, database, and priviledges" do
-    before(:each) do
-      @session = SensuPluginsOracle::Session.new(username: "a", password: "b", database: "c", module: "d", priviledge: "SYSDBA", name: "a_name")
-    end
+    let!(:session) {
+      SensuPluginsOracle::Session.new(username: "a", password: "b", database: "c", module: "d", priviledge: "SYSDBA", name: "a_name")
+    }
 
     it "creates a session" do
-      expect(@session.name).to eq("a_name")
-      expect(@session.username).to eq("a")
-      expect(@session.password).to eq("b")
-      expect(@session.database).to eq("c")
-      expect(@session.module).to eq("d")
-      expect(@session.priviledge).to eq(:SYSDBA)
+      expect(session.name).to eq("a_name")
+      expect(session.username).to eq("a")
+      expect(session.password).to eq("b")
+      expect(session.database).to eq("c")
+      expect(session.module).to eq("d")
+      expect(session.priviledge).to eq(:SYSDBA)
     end
 
     it "handles invalid aliveness" do
-      expect(@session.alive?).to eq(false)
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      expect(session.alive?).to eq(false)
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
 
     it "return error message" do
-      @session.alive?
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      session.alive?
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
 
     it "handles invalid query" do
-      expect(@session.query("select 1 from dual")).to eq(false)
-      expect(@session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
+      expect(session.query("select 1 from dual")).to eq(false)
+      expect(session.error_message).to eq("a_name: ORA-12154: TNS:could not resolve the connect identifier specified")
     end
   end
 end
